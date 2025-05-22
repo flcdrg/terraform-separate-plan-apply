3c401a726b7e647e96a0174a7a7355b1621b7a32
David Gardiner <david@gardiner.net.au>
2025-05-22 11:22:27 +0930

    Add outputs

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -15,3 +15,13 @@ resource "azurerm_storage_account" "storage" {
     "date" = var.date
   }
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_account_key" {
+  value       = azurerm_storage_account.storage.primary_access_key
+  sensitive   = true
+  description = "Primary access key for the storage account"
+}
